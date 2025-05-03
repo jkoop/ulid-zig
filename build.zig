817506b752cd150b050aada4d49edab6b11d8995
Joe Koop <joe@joekoop.com>
2025-05-02 22:26:43 -0500

    Actually make it `import`able

--- build.zig
+++ build.zig
@@ -4,26 +4,19 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
+    _ = b.addModule("ulid", .{
         .root_source_file = b.path("src/ulid.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "ulid",
-        .root_module = lib_mod,
+    const lib_tests = b.addTest(.{
+        .root_source_file = b.path("src/ulid.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-
-    b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+    test_step.dependOn(&run_lib_tests.step);
 }
